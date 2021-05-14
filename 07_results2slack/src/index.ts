3423ef72f17bfb704146f4fdd9960bc087757852
Chris Guest <chris.guest@condenastint.com>
2021-05-14 15:26:45 +0100

    Load dotenv

--- 07_results2slack/src/index.ts
+++ 07_results2slack/src/index.ts
@@ -2,6 +2,7 @@ import minimist from 'minimist';
 import { readFileSync } from 'fs';
 import * as xml2js from 'xml2js';
 import * as util from 'util';
+import * as dotenv from 'dotenv';
 
 /*
 main
@@ -9,9 +10,19 @@ main
 async function main(args: minimist.ParsedArgs) 
 {
     console.log('enter main:'+ args._);
-    console.log('results2slack')
-    // load xml file
+    dotenv.config();
+    let xml_file = process.env.XML_FILE ?? '';
+    let slack_post = process.env.SLACK_POST ?? '';
+    let slack_channel = process.env.SLACK_CHANNEL ?? '';
 
+    console.log("xml_file " + xml_file);
+    console.log("slack_post " + slack_post);
+    console.log("slack_channel " + slack_channel);
+
+    console.log('results2slack')
+
+
+    // load xml file
     const file = readFileSync('./kuttl-test.xml', 'utf-8');
     // parse the results 
     try {
