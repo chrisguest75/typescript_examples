3436d9083b163ca7b994ae5ac9c9753dfbafad5d
Chris Guest <chris.guest@condenastint.com>
2021-05-14 16:22:06 +0100

    Add templating.

--- 07_results2slack/src/index.ts
+++ 07_results2slack/src/index.ts
@@ -4,7 +4,7 @@ import * as xml2js from 'xml2js';
 import * as util from 'util';
 import * as dotenv from 'dotenv';
 import * as axios from 'axios';
-
+import * as ejs from 'ejs';
 
 /*
 main
@@ -13,10 +13,12 @@ async function main(args: minimist.ParsedArgs)
 {
     console.log('enter main:'+ args._);
     dotenv.config();
+    let environment = process.env.ENVIRONMENT ?? '';
     let xml_file = process.env.XML_FILE ?? '';
     let slack_post = process.env.SLACK_POST ?? '';
     let slack_channel = process.env.SLACK_CHANNEL ?? '';
 
+    console.log("environment " + environment);
     console.log("xml_file " + xml_file);
     console.log("slack_post " + slack_post);
     console.log("slack_channel " + slack_channel);
@@ -38,16 +40,17 @@ async function main(args: minimist.ParsedArgs)
     }
 
     // build a message
+    const template = readFileSync('./post.json', 'utf-8');
+    let details = `Environment:${environment}`
+    let results = `:x: core:coredns\n:x: core-namespaces:lighthouse\n:x: helm-tenant-app:test-service`;
+    let post = ejs.render(template, {details: details, channel: slack_channel, results: results});    
+    console.log(post);    
 
     // post to slack
-    
-    /*const post = readFileSync('./post.json', 'utf-8');
-
     axios.default.post(slack_post, post).then(res  => {
             console.log(`statusCode: ${res.status}`)
             console.log(res)
           });
-    */
     console.log('exit main');  
 
     return new Promise((resolve, reject) => {
