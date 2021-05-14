5def41ca271879c7552be41250cac2b45a6f23bc
Chris Guest <chris.guest@condenastint.com>
2021-05-14 15:10:03 +0100

    Add xml parsing

--- 07_results2slack/src/index.ts
+++ 07_results2slack/src/index.ts
@@ -1,9 +1,49 @@
 import minimist from 'minimist';
+import { readFileSync } from 'fs';
+import * as xml2js from 'xml2js';
+import * as util from 'util';
 
-function main() 
+/*
+main
+*/
+async function main(args: minimist.ParsedArgs) 
 {
-    // var a = 0
-    console.log('Hello world!')
-}
+    console.log('enter main:'+ args._);
+    console.log('results2slack')
+    // load xml file
 
-main()
+    const file = readFileSync('./kuttl-test.xml', 'utf-8');
+    // parse the results 
+    try {
+        const XML:string = file;
+        xml2js.parseString(XML, {trim: true}, function (err, result) {
+            if(err) console.log(err);
+            console.log(util.inspect(result, false, null))            
+            console.log(result); 
+        });
+    } catch (e) {
+        console.log(e);
+    }
+
+    // build a message
+
+    // post to slack
+
+
+    
+    console.log('exit main');  
+
+    return new Promise((resolve, reject) => {
+    });         
+}    
+
+/*
+Entrypoint
+*/
+let args: minimist.ParsedArgs = minimist(process.argv.slice(2));
+main(args).then(() => {
+    process.exit(0)
+}).catch((e) => {
+    console.log(e);
+    process.exit(1);
+});
