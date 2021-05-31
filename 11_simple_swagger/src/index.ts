642fdd5c5d3457cf35cde7b3d035cd2d7cdfccfa
Chris Guest <chris.guest75@hotmail.com>
2021-05-31 14:41:28 +0100

    refactored

--- 11_simple_swagger/src/index.ts
+++ 11_simple_swagger/src/index.ts
@@ -1,20 +1,15 @@
 import express, { Application } from "express";
 import pino from "express-pino-logger";
-//import { logger } from "./logger";
+import Router from "./routes/routes";
 
 const PORT = process.env.PORT || 8000;
 
 const app: Application = express();
-//app.use(express.json);
+app.use(express.json());
 //app.use(pino({ logger: logger }));
 app.use(pino());
-//app.use(express.static("public"));
-
-app.get("/ping", async (_req, res) => {
-  res.send({
-    message: "pong",
-  });
-});
+app.use(express.static("public"));
+app.use(Router);
 
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
