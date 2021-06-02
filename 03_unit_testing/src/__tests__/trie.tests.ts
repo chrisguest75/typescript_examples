294d1637968fa14869efa6dcc606de594985de33
Chris Guest <chris.guest@condenastint.com>
2021-06-02 09:18:38 +0100

    Add new tests

--- 03_unit_testing/src/__tests__/trie.tests.ts
+++ 03_unit_testing/src/__tests__/trie.tests.ts
@@ -1,43 +1,61 @@
 import { trie } from '../trie'
 
 test('empty trie contains no words', () => {
+  // ARRANGE
   let t = new trie();
+  // ACT
+  // ASSERT
   expect(t.size).toBe(0);
 });
 
-test('adding word to empty trie', () => {
+test('adding word to empty trie increase size', () => {
+  // ARRANGE
   let t = new trie();
+  // ACT
   t.add("hello");
+  // ASSERT
   expect(t.size).toBe(1);
 });
 
 test('adding word to empty trie will contain it', () => {
+  // ARRANGE
   let t = new trie();
+  // ACT
   t.add("hello");
+  // ASSERT
   expect(t.size).toBe(1);
   expect(t.contains("hello")).toBe(true);
 });
 
 test('adding empty word does not add it', () => {
+  // ARRANGE
   let t = new trie();
+  // ACT
   t.add("");
+  // ASSERT
   expect(t.size).toBe(0);
   expect(t.contains("")).toBe(false);
 });
 
 test('word not added should not be contained', () => {
+  // ARRANGE
   let t = new trie();
+  // ACT
   t.add("hello");
+  // ASSERT
   expect(t.size).toBe(1);
   expect(t.contains("hello")).toBe(true);
   expect(t.contains("bellow")).toBe(false);
 });
 
 test('add multiple words', () => {
+  // ARRANGE
   let t = new trie();
+  // ACT
   t.add("hello");
   t.add("halo");
   t.add("bell");
+  // ASSERT
   expect(t.size).toBe(3);
   expect(t.contains("hello")).toBe(true);
   expect(t.contains("halo")).toBe(true);
@@ -45,9 +63,34 @@ test('add multiple words', () => {
 });
 
 test('similiar words not added should not be found', () => {
+  // ARRANGE
   let t = new trie();
+  // ACT
   t.add("hello");
+  // ASSERT
   expect(t.size).toBe(1);
   expect(t.contains("hello")).toBe(true);
   expect(t.contains("halo")).toBe(false);
+});
+
+test('incomplete words should not be found', () => {
+  // ARRANGE
+  let t = new trie();
+  // ACT
+  t.add("hello");
+  // ASSERT
+  expect(t.size).toBe(1);
+  expect(t.contains("hello")).toBe(true);
+  expect(t.contains("helloworld")).toBe(false);
+});
+
+test('subwords not explicitly added do not exist', () => {
+  // ARRANGE
+  let t = new trie();
+  // ACT
+  t.add("hello");
+  // ASSERT
+  expect(t.size).toBe(1);
+  expect(t.contains("hello")).toBe(true);
+  expect(t.contains("he")).toBe(false);
 });
--- 03_unit_testing/src/trie.ts
+++ 03_unit_testing/src/trie.ts
@@ -14,17 +14,21 @@ class trie_node {
             // if the letter does not exists we add it
             this.nodes.set(letter, new trie_node());
         }
-        // at this point the letter should always exist
+        // at this point the letter should always exist as we have just added it if it didn't.
+        // NOTE: even though get should always return trie_node() compiler doesn't trust me
         return this.nodes.get(letter) || new trie_node();
     }
 
     contains(word: string): boolean {
         if (word.length == 1) {
+            // if word is a single letter we just check it exists
             return this.nodes.has(word);
         } else {
+            // descend through trie
             let sliced = word;
             let current_nodes: Map<string, trie_node> = this.nodes;
             for (let i: number = 0; i < sliced.length; i++) {
+                // take top letter
                 let letter = sliced.slice(0, 1);
                 sliced = sliced.slice(1);    
                 if (current_nodes.has(letter)) {
@@ -68,10 +72,10 @@ export class trie {
 
     // does the trie contain a word
     contains(word: string): boolean {
-        // words with no letters do not exist
         if (word.length > 0) {
             return this.nodes.contains(word);
         } else {
+            // words with no letters do not exist
             return false;
         }
     }    
