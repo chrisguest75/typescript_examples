8234d3d1760574e6033c28cbf4a56b53b12ee634
Chris Guest <chris.guest75@hotmail.com>
2021-11-13 20:44:17 +0000

    Add the ascify

--- 15_demoscene_banner/src/index.ts
+++ 15_demoscene_banner/src/index.ts
@@ -1,4 +1,5 @@
 import { Image } from 'image-js';
+import asciifyImage = require("asciify-image")
 
 function imageDetails(image: Image) {
     console.log('Width', image.width);
@@ -26,8 +27,8 @@ async function execute() {
 */
     let text = "carebear";
     text = text.toUpperCase();
-    const font_width=24
-    const font_height=24
+    const font_width=26
+    const font_height=26
     const chars_per_row = 12
     const banner = new Image({ width: (font_width*text.length), height: font_height});
     imageDetails(banner);
@@ -35,18 +36,34 @@ async function execute() {
     let fontImage = await Image.load('./fonts/carebear.jpg');
 
     for (let c = 0; c < text.length; c++) {
+        let letter = text[c];
         let code = text.charCodeAt(c);
         code -= " ".charCodeAt(0)
 
         let column = Math.floor(code % chars_per_row) * font_width
         let row = Math.floor(code / chars_per_row) * font_width  
 
-        let letter = fontImage.crop({x:column, y:row, width:font_width, height:font_height})
-        banner.insert(letter, {x:(c * font_width), y:0, inPlace:true})
+        let letterImage = fontImage.crop({x:column, y:row, width:font_width, height:font_height})
+        banner.insert(letterImage, {x:(c * font_width), y:0, inPlace:true})
+
+        console.log(`${code} ${column} ${row}`)
+
     }
 
-    return banner.save('./out/carebear_banner.jpg');
-  }
+    await banner.save('./out/carebear_banner.jpg');
+
+    const options = 
+
+    asciifyImage('./out/carebear_banner.jpg', {fit: 'box', width:  100, height: 26})
+        .then(function (asciified) {
+            // Print asciified image to console
+            console.log(asciified);
+        })
+        .catch(function (err: Error) {
+            // Print error to console
+            console.error(err);
+        });
+}
 
 
 function main() 
