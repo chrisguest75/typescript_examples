0cc72fa0b5fa0f27536f500c71019377bb2dfbfd
Chris Guest <chris.guest@trint.com>
2022-06-26 12:52:55 +0100

    Start to host in docker compose and send data to redis.

--- 22_websockets/ws_server/src/index.ts
+++ 22_websockets/ws_server/src/index.ts
@@ -1,6 +1,8 @@
 import { logger } from './logger'
 import * as dotenv from 'dotenv'
 import minimist from 'minimist'
+import * as readlineSync from 'readline-sync'
+import { WebSocketServer } from 'ws'
 
 /*
 main
@@ -14,8 +16,18 @@ export async function main(args: minimist.ParsedArgs) {
   logger.error('ERROR - level message')
   logger.fatal('FATAL - level message')
 
+  const wss = new WebSocketServer({ port: 40000 })
+  wss.on('connection', function connection(ws) {
+    ws.on('message', function message(data) {
+      logger.info('received: %s', data)
+    })
+
+    ws.send('something')
+  })
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   return new Promise((resolve, reject) => {
+    readlineSync.question('Quit?')
     logger.info('Finishing')
     resolve('Complete')
   })
@@ -36,3 +48,23 @@ main(args)
     logger.error(e)
     process.exit(1)
   })
+
+/*
+import { createClient } from 'redis';
+
+(async () => {
+  const client = createClient(
+    {
+      url: 'redis://0.0.0.0:6379'
+    }
+  );
+
+  client.on('error', (err) => console.log('Redis Client Error', err));
+
+  await client.connect();
+
+  await client.set('key', 'myvalue');
+  const value = await client.get('key');
+  console.log(`${value}`)
+})();
+*/
