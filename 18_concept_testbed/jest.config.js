304962924ce0ccc31a9f80078845ee4738c0976f
Chris Guest <chris.guest@trint.com>
2022-08-02 21:37:37 +0100

    Keep example of test filter.

--- 18_concept_testbed/jest.config.js
+++ 18_concept_testbed/jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  transform: {
-    '^.+\\.ts?$': 'ts-jest',
-  },
-  testEnvironment: 'node',
-  testRegex: 'tests/.*\\.(test|spec)?\\.(ts|tsx|js)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    transform: {
+        '^.+\\.ts?$': 'ts-jest',
+    },
+    testEnvironment: 'node',
+    testRegex: 'tests/.*\\.(test|spec)?\\.(ts|tsx|js)$',
+    //testRegex: 'tests/find.test.ts$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 }
