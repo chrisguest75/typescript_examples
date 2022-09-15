421eb52bd1e7d346dff660e7a0c5900c18784835
Chris Guest <chris.guest@trint.com>
2022-09-15 20:35:56 +0100

    Add more quokka pieces

--- 27_quokka/quokka-scratch.ts
+++ 27_quokka/quokka-scratch.ts
@@ -1,5 +1,24 @@
+/**
+*  ENUMS checking if a string is a value in an enum
+* 
+*/
+function IsEnumValue(theEnum, value: string) {
+    return Object.keys(theEnum).map(key => theEnum[key]).includes(value);
+}
+
+enum Color {
+    Red = "red",
+    Blue = "blue",
+    Green = "green",
+}
+
+const fromSomewhere = "green"
+let color : Color = Color[fromSomewhere];
+console.log(color)
+console.log(IsEnumValue(Color, fromSomewhere))
 
 
+// Simple functions checks
 //const voidFunction = () => void
 //console.log(voidFunction())
 
@@ -8,6 +27,8 @@ const arrowFunction = (message: string) => {
 }
 console.log(arrowFunction("hello"))
 
+
+// Checking falsy values
 const a = undefined
 const b = null
 const c = void
@@ -15,3 +36,20 @@ const c = void
 a ? console.log("a is defined") : console.log("a is undefined")
 b ? console.log("b is defined") : console.log("b is undefined")
 c ? console.log("c is defined") : console.log("c is undefined")
+
+// Checking object properties
+interface test {
+    val1?: string
+    val2?: string
+    val3?: string
+    val4?: string
+}
+
+const testObj: test = { val1: "hello", val2: undefined, val4: void 0 } } 
+
+console.log(testObj?.val1)
+
+console.log(!! testObj.val1)
+console.log(!! testObj.val2)
+console.log(!! testObj.val3)
+console.log(!! testObj.val4)
---
+++ 27_quokka/quokka-stringParsing.ts
@@ -0,0 +1,8 @@
+
+const url = "ws://machine1:9000"
+
+const urlSplit = url.split('://')[1].split(':')
+
+urlSplit
+
+//const extracted = fileName.match(/\d/g) as [];
