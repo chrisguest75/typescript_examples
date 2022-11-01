946d1cd8f80bc4a6156e1737cba9e1bcc99a58d2
Chris Guest <chrisguest75@users.noreply.github.com>
2022-11-01 09:25:56 +0000

    Some more quokka snippets.

--- 27_quokka/quoka-timeouts.ts
+++ 27_quokka/quoka-timeouts.ts
@@ -1,16 +1,24 @@
-async function sleep(ms: number) {
+import { promisify } from 'util';
+
+const sleep = promisify(setTimeout);
+
+/*async function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
-}
+}*/
+
+sleep(10000).then(() => console.log('Woken from sleep!'));
 
 const myArray = [1, 2, 3, 4, 5];
 let current = 0;
 
 const timeoutFrequency = 5000
 let watcherTimer = setTimeout(function watcher() {
+  if (current < myArray.length) {
     const value = myArray[current];
     console.log({ state: 'watcher', value });
     current++
-    watcherTimer = setTimeout(watcher, timeoutFrequency)
+    watcherTimer = setTimeout(watcher, timeoutFrequency);
+  } else {
+    console.log({ state: 'watcher', value: 'done' });
+  } 
 }, timeoutFrequency)
-
-
--- 27_quokka/quokka-json.ts
+++ 27_quokka/quokka-json.ts
@@ -1,10 +1,23 @@
-const chainedString = '{ "chain": [ {"url":"http://nginx:80/a/fetch", "payload":{"url":"http://nginx:80/b/ping", "payload":{} } }, {"url":"http://nginx:80/b/ping"}, {"url":"http://nginx:80/a/ping"} ] }' 
+//const chainedString = '{ "chain": [ {"url":"http://nginx:80/b/ping"}, {"url":"http://nginx:80/a/fetch", "payload":{"url":"http://nginx:80/b/ping", "payload":{} } }, {"url":"http://nginx:80/a/ping"} ] }' 
 //const chainedString = '{ "chain": [ {"url":"http://nginx:80/a/fetch", "payload":"{\\"url\\":\\"http://nginx:80/b/ping\\", \\"}" }, {"url":"http://nginx:80/b/ping"} ] }' 
+const chainedString = '{ "chain": [ {"url":"http://nginx:80/b/ping"}, {"url":"http://nginx:80/a/fetch", "payload":{ "chain": [ {"url":"http://nginx:80/b/ping" } ] }}, {"url":"http://nginx:80/a/ping"} ] }'
 
 const chained = JSON.parse(chainedString)
 console.log(chained)
+
+chained.chain.map((item) => {
+    console.log(item)
+    if (item.payload) {
+        console.log(JSON.stringify(item.payload))
+    }
+})
+
 
 const first = chained.chain.shift()
 console.log(first)
 console.log(chained.chain)
 console.log(chained)
+
+// with first url removed
+console.log(JSON.stringify(chained))
+
