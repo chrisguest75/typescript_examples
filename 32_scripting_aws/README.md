fd27d965f1c982f635aab554e3fe96d82b817dc9
Chris Guest <chrisguest75@users.noreply.github.com>
2023-01-18 21:01:42 +0000

    Configure a simple quokka for testing.

---
+++ 32_scripting_aws/README.md
@@ -0,0 +1,43 @@
+# README
+
+Demonstrate examples of using TS as a language for specific 
+
+## Reason
+
+
+
+## Install
+
+```sh
+# install extension
+code --install-extension WallabyJs.quokka-vscode
+```
+
+```sh
+# install packages for the scripts
+npm install
+```
+
+## Run
+
+```sh
+# execute the script
+Cmd+Shift+P - Quokka.js Start on Current File
+```
+
+## Creating typescript types
+
+It's possible to create typescript types from javascript objects.
+
+```sh
+# create typescript types from javascript objects
+code --install-extension quicktype.quicktype
+```
+
+## Resources
+
+https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/
+https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-ecs/index.html
+https://github.com/aws/aws-sdk-js
+https://github.com/aws-samples/aws-sdk-js-tests
+https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-servicediscovery/index.html
---
+++ 32_scripting_aws/quokka-find-ecs-tasks.ts
@@ -0,0 +1,50 @@
+import { ECSClient, ListClustersCommand, ListTasksCommand, DescribeTasksCommand } from '@aws-sdk/client-ecs'
+import { fromIni } from '@aws-sdk/credential-providers'
+import { promisify } from 'util'
+
+const region = process.env.REGION || 'us-east-1'
+const clusterName = process.env.CLUSTERNAME || 'default'
+const profile = process.env.AWS_PROFILE || 'default'
+
+const credentials = fromIni({ profile })
+
+async function allClusters(region: string) {
+  const client = new ECSClient({ region: region })
+  const clusterCommand = new ListClustersCommand({})
+  const clusterResponse = await client.send(clusterCommand)
+  return new Promise((resolve, reject) => {
+    resolve(clusterResponse)
+  })
+}
+
+async function allTasks(region: string, clusterName: string): Promise<string[]> {
+  const client = new ECSClient({ region: region })
+  let tasksCommand = new ListTasksCommand({ cluster: clusterName })
+  let tasksResponse = await client.send(tasksCommand)
+
+  // list all tasks in the cluster
+  let allTasks: string[] = []
+  allTasks.push(...tasksResponse.taskArns)
+  while (tasksResponse.nextToken !== undefined) {
+    tasksCommand = new ListTasksCommand({ cluster: clusterName, nextToken: tasksResponse.nextToken })
+    tasksResponse = await client.send(tasksCommand)
+    allTasks.push(...tasksResponse.taskArns)
+  }
+  console.log(allTasks.length)
+  return new Promise((resolve, reject) => {
+    resolve(allTasks)
+  })
+}
+
+async function test() {
+  const clusters = await allClusters(region)
+  console.log(clusters)
+
+  const tasks = await allTasks(region, clusterName)
+  const client = new ECSClient({ region: region })
+  const describeCommand = new DescribeTasksCommand({ tasks: tasks, cluster: clusterName })
+  const describeResponse = await client.send(describeCommand)
+  console.log(describeResponse.tasks?.length)
+}
+
+test()
