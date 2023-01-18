3cc6bbea095a0171e7812d3431d1d0a150f61a65
Chris Guest <chrisguest75@users.noreply.github.com>
2023-01-18 20:22:30 +0000

    Upgrade cli to node18 lts

---
+++ 32_scripting_aws/src/index.ts
@@ -0,0 +1,61 @@
+import { logger } from './logger'
+import * as dotenv from 'dotenv'
+import minimist from 'minimist'
+
+/*
+main
+*/
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function main(args: minimist.ParsedArgs) {
+  logger.trace('TRACE - level message')
+  logger.debug('DEBUG - level message')
+  logger.info('INFO - level message')
+  logger.warn('WARN - level message')
+  logger.error('ERROR - level message')
+  logger.fatal('FATAL - level message')
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  return new Promise((resolve, reject) => {
+    logger.info('Finishing')
+    resolve('Complete')
+  })
+}
+
+process.on('exit', async () => {
+  logger.warn('exit signal received')
+  process.exit(1)
+})
+
+process.on('uncaughtException', async (error: Error) => {
+  logger.error(error)
+  // for nice printing
+  console.log(error)
+  process.exit(1)
+})
+
+process.on('unhandledRejection', async (reason, promise) => {
+  logger.error({
+      promise: promise,
+      reason: reason,
+      msg: 'Unhandled Rejection',
+  })
+  console.log(reason)
+  process.exit(1)
+})
+
+
+/*
+Entrypoint
+*/
+// load config
+dotenv.config()
+logger.info(`Pino:${logger.version}`)
+const args: minimist.ParsedArgs = minimist(process.argv.slice(2))
+main(args)
+  .then(() => {
+    process.exit(0)
+  })
+  .catch((e) => {
+    logger.error(e)
+    process.exit(1)
+  })
