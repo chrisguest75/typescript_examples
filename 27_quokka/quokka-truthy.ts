a3b56068ff3be5697392dd5cf31facdda738e0b9
Chris Guest <chrisguest75@users.noreply.github.com>
2023-02-07 17:49:37 +0000

    Quick truthy tests.

---
+++ 27_quokka/quokka-truthy.ts
@@ -0,0 +1,14 @@
+// https://developer.mozilla.org/en-US/docs/Glossary/Truthy
+
+undefined ? console.log('undefined is true') : console.log('undefined is false')
+null ? console.log('null is true') : console.log('null is false')
+
+
+'' ? console.log('empty string is true') : console.log('empty string is false')
+'hello' ? console.log('hello is true') : console.log('hello is false')
+
+0 ? console.log('0 is true') : console.log('0 is false')
+1 ? console.log('1 is true') : console.log('1 is false')
+
+
+
