c256350e316b006d3df04ce24f5c69ab73e3f9ab
Chris Guest <chrisguest75@users.noreply.github.com>
2023-02-07 13:51:21 +0000

    Remove junk

--- 27_quokka/quokka-modifyEnvPath.ts
+++ 27_quokka/quokka-modifyEnvPath.ts
@@ -7,16 +7,12 @@ async function modifyEnvPath() {
   //testVar=''
   console.log(testVar)
 
+  // filter and add the path
   const newPath = `${testVar.split(':').filter((path) => path !== '/opt/sox/libs/lib64').join(':')}:/opt/sox/libs/lib64`
   console.log(newPath)
   process.env.LD_LIBRARY_PATH=newPath
 
   console.log(process.env.LD_LIBRARY_PATH)
-
-  /*process.env
-  process.env['LD_LIBRARY_PATH'] =
-    (process.env['LD_LIBRARY_PATH'] || '') + ':/opt/sox/libs/lib64';
-  this.logger.info(process.env.LD_LIBRARY_PATH);*/
 }
 
 modifyEnvPath()
