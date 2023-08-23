d3969eb3347b19d17e84b6de6b22169e22963c48
Chris Guest <chrisguest75@users.noreply.github.com>
2023-08-23 16:08:48 +0100

    Add a better destructuring and spread example

--- 27_quokka/quokka-destructuring.ts
+++ 27_quokka/quokka-destructuring.ts
@@ -1,15 +1,18 @@
+
 // Destructuring
-function testOne() {
+function testBasicRemaining() {
     const testOne = { a: 1, b: 2, c: 3, d: 4, e: 5 }
     const { a, b, ...rest } = testOne
     console.log(a)
     console.log(b)
+    // rest takes the remaining properties
     console.log(rest)    
 }
 
 // Overriding none existing properties
-function testTwoDefalts() {
+function testDestructuringDefaults() {
     const testOne = { a: 1, b: 2, c: 3, d: 4, e: 5 }
+    // z does not exist in testOne
     const { a, b=60, z=40, ...rest } = testOne
     console.log(a)
     console.log(b)
@@ -17,11 +20,8 @@ function testTwoDefalts() {
     console.log(rest)    
 }
 
-testOne()
-testTwoDefalts()
-
 // nested destructuring with renaming
-function testThree() {
+function testNestingWithRename() {
     const obj = {
         a: 1,
         b: {
@@ -33,11 +33,30 @@ function testThree() {
     const { a, b: { c, d:chris, } } = obj;
     console.log(a, c, chris);
 }
-testThree()
 
-function testFour({b}: any) {
+// destructuring function parameter any type
+function testDestructureInputs({b}: any) {
+    // only b is destructured from the input object
     console.log(b)
 }
-testFour({ a: 1, b: 2, text: "hello" })
 
+
+function testConditionalSpread() {
+    // if set in environment, use it, otherwise ignore
+    const buildId = process.env.BUILDID;
+    //const buildId = 333;
+    const testOne = { a: 1, b: 2, c: 3, d: 4, e: 5 }
+    const final = { ...testOne, ...(buildId ? { buildId } : {})}
+
+    console.log(final)
+}
+
+/********************************
+ * 
+ */
+testBasicRemaining()
+testDestructuringDefaults()
+testNestingWithRename()
+testDestructureInputs({ a: 1, b: 2, text: "hello" })
+testConditionalSpread()
 
