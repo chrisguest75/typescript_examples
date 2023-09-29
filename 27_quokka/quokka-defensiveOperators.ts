8ab147bad4670c25c0c1ea90505a15e0b9e2490c
Chris Guest <chrisguest75@users.noreply.github.com>
2023-09-29 18:22:00 +0100

    Add a few quokka examples

---
+++ 27_quokka/quokka-defensiveOperators.ts
@@ -0,0 +1,28 @@
+// Testing some defensive programming techniques
+const myObject = {
+  values: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+  empty: [],
+  novalues: undefined,
+};
+
+// A values array with length > 0
+const test1 = myObject.values.length > 0 ? myObject.values.length : undefined;
+console.log(test1);
+
+// Stupid test I know
+const test2 = myObject.empty.length > 0 ? myObject.empty.length : 0;
+console.log(test2);
+
+// An undefined value with length > 0. But guarded by the ? operator
+// Up to implementation to decide semnatics of undefined length or default 0.
+const test3 =
+  myObject.novalues?.length > 0 ? myObject.novalues.length : undefined;
+console.log(test3);
+
+// An undefined value with length > 0. But guarded by the && operator
+// Is this better?
+const test4 =
+  myObject.novalues && myObject.novalues.length > 0
+    ? myObject.novalues.length
+    : undefined;
+console.log(test4);
--- 27_quokka/quokka-truthy.ts
+++ 27_quokka/quokka-truthy.ts
@@ -1,14 +1,39 @@
 // https://developer.mozilla.org/en-US/docs/Glossary/Truthy
 
-undefined ? console.log('undefined is true') : console.log('undefined is false')
-null ? console.log('null is true') : console.log('null is false')
+// undefined is false
+undefined
+  ? console.log("undefined is true")
+  : console.log("undefined is false");
 
+// null is false
+null ? console.log("null is true") : console.log("null is false");
 
-'' ? console.log('empty string is true') : console.log('empty string is false')
-'hello' ? console.log('hello is true') : console.log('hello is false')
+// STRINGS
+// empty string is false
+"" ? console.log("empty string is true") : console.log("empty string is false");
+// hello is true
+"hello" ? console.log("hello is true") : console.log("hello is false");
 
-0 ? console.log('0 is true') : console.log('0 is false')
-1 ? console.log('1 is true') : console.log('1 is false')
+// NUMBERS
+// 0 is false
+0 ? console.log("0 is true") : console.log("0 is false");
+// 1 is true
+1 ? console.log("1 is true") : console.log("1 is false");
 
-
-
+// COMPARISON
+const a = undefined;
+console.log(a);
+// undefined is not greater then 0
+console.log(a > 0);
+// undefined is not less then 0
+console.log(a < 0);
+// equality of undefined
+console.log(a == a);
+console.log(a === a);
+// adition
+console.log(a + a);
+const b = {};
+console.log(b);
+// objects
+console.log(b.test ? b.test : 0);
+console.log(b?.test ? b.test : 0);
