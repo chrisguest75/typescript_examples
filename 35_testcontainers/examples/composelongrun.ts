4c526b1a7e12574d9f8365b0b75e5878678cbfb1
Chris Guest <chrisguest75@users.noreply.github.com>
2024-05-01 15:11:51 +0100

    feat(testcontainers): :sparkles: Use docker compose testcontainers with podinfo and a custom long run process.

---
+++ 35_testcontainers/examples/composelongrun.ts
@@ -0,0 +1,22 @@
+import { logger } from '../src/logger.js'
+import path from 'path'
+import { DockerComposeEnvironment, Wait } from 'testcontainers'
+
+export default async function compose() {
+  const cwd = process.cwd()
+  const environment = await new DockerComposeEnvironment(path.join(cwd, 'data/compose'), 'docker-compose.longrun.yaml')
+  environment.withStartupTimeout(5000)
+  environment.withWaitStrategy('longrun-1', Wait.forLogMessage('--Ready--'))
+  //environment.withWaitStrategy('longrun', Wait.forListeningPorts())
+  const live = await environment.up(['longrun'])
+  logger.info('Started ')
+
+  const startedContainer = live.getContainer('longrun-1')
+  const logs = await startedContainer.logs()
+  logs.addListener('data', (line) => logger.info(line))
+
+  // sleep
+  await new Promise((resolve) => setTimeout(resolve, 5000))
+
+  live.stop()
+}
